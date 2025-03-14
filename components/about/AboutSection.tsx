966c831bd95d4eba92bad70ae5bd0e7293cbcd79
kwasi <affulnana04@gmail.com>
2025-03-14 17:55:45 +0000

    increase the text sizes and padding on the about section

--- components/about/AboutSection.tsx
+++ components/about/AboutSection.tsx
@@ -2,17 +2,21 @@
 
 const AboutSection = () => {
   return (
-    <section className="w-[85%] container mx-auto py-12 border-b border-gray-300">
+    <section className="w-[85%] container mx-auto pb-12 pt-[120px] border-b border-gray-300 text-[16px]">
       <div className="grid grid-cols-1 md:grid-cols-[0.5fr_1fr] gap-6 items-center">
         {/* Title */}
-        <h2 className="text-3xl font-semibold text-black">About University</h2>
+        <h2 className="text-4xl font-semibold text-black">About University</h2>
 
         {/* Separator & Text */}
-        <div className="flex space-x-6">
+        <div className="flex gap-[50px] ">
           <div className="border-l border-gray-300"></div>
-          <p className="text-gray-600 leading-relaxed">
-            Welcome to <span className="font-medium">Unipix University</span>, where knowledge meets inspiration, and every individual’s educational journey is valued. 
-            Established in <span className="font-medium">1971</span>, our university has been a bastion of learning, innovation, and community for <span className="font-medium">51 years</span>.
+          <p className="text-[#737477] leading-relaxed">
+            Welcome to <span className="font-medium">Unipix University</span>,
+            where knowledge meets inspiration, and every individual’s
+            educational journey is valued. Established in{" "}
+            <span className="font-medium">1971</span>, our university has been a
+            bastion of learning, innovation, and community for{" "}
+            <span className="font-medium">51 years</span>.
           </p>
         </div>
       </div>
