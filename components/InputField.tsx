7ed378dbeeb4f0490026c310f41daa0427afde0d
kwasi <affulnana04@gmail.com>
2025-03-15 21:25:02 +0000

    fix bug on input field

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -4,20 +4,16 @@ const InputField = ({
   label,
   name,
   type = "text",
-  value,
-  onChange,
   error,
   placeholder,
+  ...rest
 }: {
   label: string;
   name: string;
   type?: string;
-  value: string;
-  onChange: (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => void;
-  error: boolean;
-  placeholder: string;
+  error?: string;
+  placeholder?: string;
+  [key: string]: any;
 }) => (
   <div>
     <label className="block text-sm font-semibold text-black">
@@ -26,14 +22,11 @@ const InputField = ({
     <input
       type={type}
       name={name}
-      value={value}
-      onChange={onChange}
       placeholder={placeholder}
-      className={`w-full border py-3 px-[20px] mt-3 ${
-        error
-          ? "border-red-500"
-          : "border-[#ddd] focus:border-[#890c25] outline-0"
+      className={`w-full border py-3 px-[20px] mt-3 focus:border-[#890c25] outline-0 ${
+        error ? "border-red-500" : "border-[#ddd] "
       }`}
+      {...rest}
     />
     {error && (
       <p className="text-red-500 text-sm mt-1">This field is required.</p>
