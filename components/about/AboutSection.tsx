068d92515289fbe10e35614cfb7d074f82fb749d
kwasi <affulnana04@gmail.com>
2025-03-15 20:57:25 +0000

    change  font weight on about section

--- components/about/AboutSection.tsx
+++ components/about/AboutSection.tsx
@@ -5,12 +5,12 @@ const AboutSection = () => {
     <section className="w-[85%] container mx-auto pb-12 pt-[120px] border-b border-gray-300 text-[16px]">
       <div className="grid grid-cols-1 md:grid-cols-[0.5fr_1fr] gap-6 items-center">
         {/* Title */}
-        <h2 className="text-4xl font-semibold text-black">About University</h2>
+        <h2 className="text-4xl text-black">About University</h2>
 
         {/* Separator & Text */}
         <div className="flex gap-[50px] ">
           <div className="border-l border-gray-300"></div>
-          <p className="text-[#737477] leading-relaxed">
+          <p className="leading-relaxed">
             Welcome to <span className="font-medium">Unipix University</span>,
             where knowledge meets inspiration, and every individual’s
             educational journey is valued. Established in{" "}
