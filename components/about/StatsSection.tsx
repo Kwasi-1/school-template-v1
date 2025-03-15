9d2100445a9059e71df6564a76404b6ce0613045
kwasi <affulnana04@gmail.com>
2025-03-15 21:17:05 +0000

    improve typography and layout on the stats card

--- components/about/StatsSection.tsx
+++ components/about/StatsSection.tsx
@@ -12,13 +12,15 @@ const StatsSection = () => {
       <div className="bg-[#7F0019] text-white py-8 px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 text-center items-center gap-6">
           {stats.map((stat, index) => (
-            <div key={index} className="relative">
-              <h2 className="text-5xl font-semibold">{stat.value}</h2>
-              <p className="text-sm mt-2">{stat.label}</p>
+            <div key={index} className="flex flex-col relative justify-end">
+              <div className=" w-1/2 mx-auto text-left">
+                <h2 className="text-5xl pb-4">{stat.value}</h2>
+                <p className="text-base mt-2 leading-8">{stat.label}</p>
+              </div>
 
               {/* Divider (hidden on mobile, shown on larger screens) */}
               {index !== stats.length - 1 && (
-                <div className="hidden md:block absolute top-1/2 right-0 transform -translate-y-1/2 h-12 border-r border-white"></div>
+                <div className="hidden md:block absolute top-1/2 right-0 transform -translate-y-1/2 h-24 border-r border-red-400/50"></div>
               )}
             </div>
           ))}
